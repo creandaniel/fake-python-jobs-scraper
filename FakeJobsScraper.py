242756ef3be638e6ed26fe129b8d6d8a3257afc6
Daniel Crean <creandaniel@hotmail.com>
2021-11-15 22:53:15 +0000

    Update FakeJobsScraper.py
    Improve data structure of data into dictonary

--- FakeJobsScraper.py
+++ FakeJobsScraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from time import sleep
 
 class FakeJobsScraper:
     def __init__ (self):
@@ -12,24 +13,26 @@ class FakeJobsScraper:
         self.job_elements = soup.find_all("div", class_="card-content")
 
         for job_element in self.job_elements:
-            self.title_element = job_element.find("h2", class_="title")
-            self.company_element = job_element.find("h3", class_="subtitle")
-            self.date_element = job_element.find("p", class_="is-small")
-            print(
-                  "Job Title: " + self.title_element.text.strip() + 
-                  " Company: " + self.company_element.text.strip() + 
-                  " Date Posted: " + self.date_element.text.strip()
-                 )
-        
+
+            d = dict()
+            d['title_element'] = job_element.find("h2", class_="title")
+            d['company_element'] = job_element.find("h3", class_="subtitle")
+            d['job_element'] = job_element.find("p", class_="is-small")
+
+            data = []
+            data.append(d)
+            
+            self.display_data(data)
+
+
+        # sleep(10)
+    def display_data(self, data):
+        for d in data:
+            print(d['title_element'])
+            print(d['company_element'])
+            print(d['job_element'])
+
 
 s = FakeJobsScraper()
 
 print(s.scrape_data())
-
-
-    
-
-
-
-
-
