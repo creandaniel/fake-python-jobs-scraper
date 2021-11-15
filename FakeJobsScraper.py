c7a443573aab8fd5cb98ebc57cc94cded29aa5ba
Daniel Crean <creandaniel@hotmail.com>
2021-11-15 23:56:26 +0000

    Update FakeJobsScraper.py
    exception handling when scraping url

--- FakeJobsScraper.py
+++ FakeJobsScraper.py
@@ -1,28 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.error import URLError
 from time import sleep
+import json
 
 class FakeJobsScraper:
     def __init__ (self):
         self.url = "https://realpython.github.io/fake-jobs"
        
     def scrape_data(self):
-        req = requests.get(self.url)
-        soup = BeautifulSoup(req.text, "html.parser")
+        try:
+            req = requests.get(self.url)
+            req.raise_for_status()
+        except URLError as ue:
+            print("The Server Could Not be Found")
+        else: 
 
-        self.job_elements = soup.find_all("div", class_="card-content")
+            soup = BeautifulSoup(req.text, "html.parser")
 
-        for job_element in self.job_elements:
+            self.job_elements = soup.find_all("div", class_="card-content")
 
-            d = dict()
-            d['title_element'] = job_element.find("h2", class_="title")
-            d['company_element'] = job_element.find("h3", class_="subtitle")
-            d['job_element'] = job_element.find("p", class_="is-small")
+            for job_element in self.job_elements:
 
-            data = []
-            data.append(d)
-            
-            self.display_data(data)
+                d = dict()
+                d['title_element'] = job_element.find("h2", class_="title").text.strip()
+                d['company_element'] = job_element.find("h3", class_="subtitle").text.strip()
+                d['job_element'] = job_element.find("p", class_="is-small").text.strip()
+                
+                data = []
+                data.append(d)
+                #self.save_json(data)
+                self.display_data(data)
 
 
         # sleep(10)
@@ -31,8 +39,20 @@ class FakeJobsScraper:
             print(d['title_element'])
             print(d['company_element'])
             print(d['job_element'])
-
+    
+    
+    # def save_json(self,data):
+    #     with open('allsides.json', 'w') as f:
+    #         json.dump(data, f)
 
 s = FakeJobsScraper()
 
 print(s.scrape_data())
+
+
+    
+
+
+
+
+
