a0cc781aebee682b2e3f767ac880533b45eaa973
Daniel Crean <creandaniel@hotmail.com>
2021-11-19 22:56:37 +0000

    get jobs in json file as an cache or scrap

--- FakeJobsScraper.py
+++ FakeJobsScraper.py
@@ -3,10 +3,13 @@ import requests
 from urllib.error import URLError
 from time import sleep
 import json
+import os.path
 
 class FakeJobsScraper:
     def __init__ (self):
         self.url = "https://realpython.github.io/fake-jobs"
+
+
        
     def scrape_data(self):
         try:
@@ -19,7 +22,7 @@ class FakeJobsScraper:
             soup = BeautifulSoup(req.text, "html.parser")
 
             self.job_elements = soup.find_all("div", class_="card-content")
-
+            data = []
             for job_element in self.job_elements:
 
                 d = dict()
@@ -27,10 +30,9 @@ class FakeJobsScraper:
                 d['company_element'] = job_element.find("h3", class_="subtitle").text.strip()
                 d['job_element'] = job_element.find("p", class_="is-small").text.strip()
                 
-                data = []
                 data.append(d)
-                #self.save_json(data)
-                self.display_data(data)
+                self.save_json(data)
+                #self.display_data(data)
 
 
         # sleep(10)
@@ -41,18 +43,26 @@ class FakeJobsScraper:
             print(d['job_element'])
     
     
-    # def save_json(self,data):
-    #     with open('allsides.json', 'w') as f:
-    #         json.dump(data, f)
+    def save_json(self,data):
+        with open("textbooks.json", "w") as writeJSON:
+            json.dump(data, writeJSON, ensure_ascii=False, indent=4)
+
+    def open_json(self):
+        f = open('textbooks.json',)
+        data = json.load(f)
+
+        for i in data:
+            print(i['title_element'])
+            print(i['company_element'])
+            print(i['job_element'])
+
+        f.close()
 
 s = FakeJobsScraper()
 
-print(s.scrape_data())
-
-
-    
-
-
-
-
-
+if os.path.exists('textbooks.json'):
+    print(s.open_json())
+    print("file exsists")
+else:
+    print(s.scrape_data())
+    print("file doesn't exsist so we wcrape the data")
